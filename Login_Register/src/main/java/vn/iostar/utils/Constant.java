dfe2914de41c39d51b320a13f4827724d1a93eb6
nnhhlkj123 <157661854+nnhhlkj123@users.noreply.github.com>
2024-09-16 18:23:57 +0700

    Add files via upload

---
+++ Login_Register/src/main/java/vn/iostar/utils/Constant.java
@@ -0,0 +1,7 @@
+package vn.iostar.utils;
+
+public class Constant {
+	public static final String REGISTER = "/views/register.jsp";
+	public static final String SESSION_USERNAME = "username";
+	public static final String COOKIE_REMEMBER = "username";
+}
